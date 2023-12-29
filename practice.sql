41781effaf52c1184ae570316b2030c744d1f1c5
mohanrathode1@gmail.com <mohanrathode1@gmail.com>
2023-12-29 15:46:10 +0000

    new changes 2

---
+++ practice.sql
@@ -0,0 +1,19 @@
+{{ config(materialized="table") }}
+
+
+with
+    cte as (
+        select
+            customer_id,
+            order_date,
+            dense_rank() over (
+                partition by customer_id order by customer_id, order_date
+            ) drank
+        from mohan.practice.orderss
+    )
+select
+    order_date,
+    sum(case when drank = 1 then 1 else 0 end) new_user,
+    sum(case when drank > 1 then 1 else 0 end) repeated_user
+from cte
+group by order_date
